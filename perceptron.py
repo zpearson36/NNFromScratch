20d910938ab8a5df9895e38f6154bc10c302ea8e
Zach <dudefro3600@gmail.com>
2024-03-07 18:37:34 -0600

    updated perceptron to have a bias variable (set to 1), data classification can do arbitrary lines, added line function to define line

--- perceptron.py
+++ perceptron.py
@@ -4,12 +4,13 @@ import numpy as np
 
 class Perceptron:
     def __init__(self, activation_function, learning_rate):
-        self.weights = np.empty(2)
+        self.weights = np.random.uniform(-1, 1, 2)
         self.af = activation_function
         self.learning_rate = learning_rate
+        self.bias = 1
 
     def forward(self, inputs):
-        self.output = [np.dot(inputs, self.weights.T)]
+        self.output = [np.dot(inputs, self.weights.T) + self.bias]
         self.af.forward(self.output)
         self.output = self.af.output
 
@@ -37,20 +38,35 @@ if __name__ == "__main__":
     _af = af.ActivationFunction("step")
     perceptron = Perceptron(_af, .1)
 
-    data_set = np.random.random((1000, 2)) * 100
+    data_set = np.random.uniform(-1, 1, (1000, 2)) * 100
 
-    def data_classification(data):
+    def line(x,
+             slope = np.random.uniform(-20, 20, 1)[0],
+             intercept = np.random.uniform(-50, 50, 1)[0]
+             ):
+        return slope * x + intercept
+
+    def data_classification(data, line):
+        '''
+        data set is a collection of 2 dimensional points.
+        Classifies each point on whether they lay above or
+        below a line defined by the values of slope and
+        intercept.
+
+        if below line, class=1
+        if above or on line, class=0
+        '''
         classified_data = []
         for point in data:
             classification = [point]
-            if point[0] > point[1]:
+            if point[1] > line(point[0]):
                 classification.append(1)
             else:
                 classification.append(0)
             classified_data.append(classification)
         return classified_data
 
-    classified_data = data_classification(data_set)
+    classified_data = data_classification(data_set, line)
 
     correct, incorrect = perceptron.test(classified_data[800:])
     print("========Before Training=======")
